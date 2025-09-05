e20c6b0e1107f0c470e2a6932a833ec8e4583ee0
noelmcloughlin <noel.mcloughlin@gmail.com>
2025-09-05 15:54:04 +0100

    chore(dev): handle newbie db-not-setup error gracefully

--- src/app/[locale]/(public)/page.jsx
+++ src/app/[locale]/(public)/page.jsx
@@ -19,34 +19,54 @@ export default async function Home({ params : { locale } }) {
 
   setLocaleCache(locale);
 
-  // Querying for select2 list initial options
-  const territoryOptions = await db.selectFrom('Entry')
-    .where('category', '=', 'territories')
-    .where('published', '=', true)
-    .select(['id', 'name'])
-    .limit(25)
-    .execute()
+  try {
+    // Querying for select2 list initial options
+    const territoryOptions = await db.selectFrom('Entry')
+      .where('category', '=', 'territories')
+      .where('published', '=', true)
+      .select(['id', 'name'])
+      .limit(25)
+      .execute()
 
-  const languageOptions = await db.selectFrom('Entry')
-    .where('category', '=', 'languages')
-    .where('published', '=', true)
-    .select(['id', 'name'])
-    .limit(25)
-    .execute()
+    const languageOptions = await db.selectFrom('Entry')
+      .where('category', '=', 'languages')
+      .where('published', '=', true)
+      .select(['id', 'name'])
+      .limit(25)
+      .execute()
 
-  const treatyOptions = await db.selectFrom('Entry')
-    .where('category', '=', 'treaties')
-    .where('published', '=', true)
-    .select(['id', 'name'])
-    .limit(25)
-    .execute()
+    const treatyOptions = await db.selectFrom('Entry')
+      .where('category', '=', 'treaties')
+      .where('published', '=', true)
+      .select(['id', 'name'])
+      .limit(25)
+      .execute()
 
-  return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <MapModal headerText="disclaimer-header" bodyText="disclaimer" footerText="disclaimer-close" />
-      <MapContainer territoryOptions={territoryOptions} languageOptions={languageOptions} treatyOptions={treatyOptions} />
-      <FrontPage />
-      <AIChatbot />
+    return (
+      <div className="font-[family-name:var(--font-geist-sans)]">
+        <MapModal headerText="disclaimer-header" bodyText="disclaimer" footerText="disclaimer-close" />
+        <MapContainer territoryOptions={territoryOptions} languageOptions={languageOptions} treatyOptions={treatyOptions} />
+        <FrontPage />
+        <AIChatbot />
+      </div>
+    );
+  } catch (err) {
+    const isDev = process.env.NODE_ENV === 'development';
+
+    // handle startup errors gracefully
+    console.log(err);
+    if (err.message.includes('relation "Entry" does not exist')) {
+      console.error('\x1b[31m%s\x1b[0m', "Ensure database is set up correctly, and/or seeded.");
+    }
+    // Return a error UI
+    return (<div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Error</h1>
+      {isDev ? (
+        <p className="text-red-600">An error occurred while loading the page. Please review console errors.</p>
+      ) : (
+        <p className="text-red-600">An error occurred while loading the page. Please try again later.</p>
+      )}
     </div>
-  );
+    );
+  }
 }
--- src/components/static/sidebar.jsx
+++ src/components/static/sidebar.jsx
@@ -8,22 +8,42 @@ export default async function Sidebar({ children = (<div></div>), picks = 5 }) {
 
   const t = await getTranslations('Sidebar');
 
-  const totalEntries = await db.selectFrom('Entry')
-    .select((eb) => eb.fn.count('id').as('num_entries'))
-    .where('Entry.category', '!=', "placenames")
-    .execute();
+  try {
+    const totalEntries = await db.selectFrom('Entry')
+      .select((eb) => eb.fn.count('id').as('num_entries'))
+      .where('Entry.category', '!=', "placenames")
+      .execute();
 
-  const randomIndex = Math.floor(Math.random() * (totalEntries[0].num_entries - picks));
+    const randomIndex = Math.floor(Math.random() * (totalEntries[0].num_entries - picks));
 
-  let entries = await db.selectFrom('Entry')
-    .leftJoin('Media', 'Media.entryId', 'Entry.id')
-    .select(['Entry.id as id', 'Entry.name as name', 'Entry.category as category', 'Entry.slug as slug', 'Entry.updatedAt as updatedAt', 'Media.url as media_url'])
-    .where('Entry.category', '!=', "placenames")
-    .orderBy('Entry.color', 'asc')
-    .limit(picks)
-    .offset(randomIndex)
-    .execute()
+    let entries = await db.selectFrom('Entry')
+      .leftJoin('Media', 'Media.entryId', 'Entry.id')
+      .select(['Entry.id as id', 'Entry.name as name', 'Entry.category as category', 'Entry.slug as slug', 'Entry.updatedAt as updatedAt', 'Media.url as media_url'])
+      .where('Entry.category', '!=', "placenames")
+      .orderBy('Entry.color', 'asc')
+      .limit(picks)
+      .offset(randomIndex)
+      .execute()
 
+  } catch (err) {
+    const isDev = process.env.NODE_ENV === 'development';
+
+    // handle startup errors gracefully
+    console.log(err);
+    if (err.message.includes('relation "Entry" does not exist')) {
+      console.error('\x1b[31m%s\x1b[0m', "Ensure database is set up correctly, and/or seeded.");
+    }
+    // Return a error UI
+    return (<div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Error</h1>
+      {isDev ? (
+        <p className="text-red-600">An error occurred while loading the page. Please review console errors.</p>
+      ) : (
+        <p className="text-red-600">An error occurred while loading the page. Please try again later.</p>
+      )}
+    </div>
+    );
+  }
 
   return (
     <div className="col-span-1 bg-white rounded-t shadow-lg p-4 mt-5 order-last md:order-first">
